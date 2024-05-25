601a341ddb376542524488cdeb5d51698a5116c4
Lerzan Seyma <lerzanseyma@gmail.com>
2024-05-25 20:49:53 +0300

    v4

---
+++ src/main/java/Runner.java
@@ -0,0 +1,19 @@
+public class Runner {
+
+    public static void main(String[] args) {
+
+        // versiyon 1
+        System.out.println("hello GIT");
+        System.out.println("git add . : staging areaya alir.");
+
+        //versiyon 4
+        System.out.println("hi");
+
+
+
+
+
+
+
+    }
+}
